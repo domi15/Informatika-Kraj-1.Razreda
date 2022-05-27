47a01b1dd9782af4f5c3159cd998fbf74fe54e94
domi15 <81708998+domi15@users.noreply.github.com>
2022-05-27 16:35:11 +0200

    Add files via upload

---
+++ js.js
@@ -0,0 +1,11 @@
+function WhatTheDogDoing() {
+    n = prompt('Unesi duljinu stranice A')
+    var m = n
+    document.getElementById('duljina').innerHTML = m
+    document.getElementById('povrsina').innerHTML = m*m
+    document.getElementById("a").innerHTML = "Sada ćemo podijeliti kvadrat na četiri jednaka dijela"
+    document.getElementById("b").innerHTML = "Možemo vidjeti kako stranice AE i AH sada tangente kružnice te kako su obje duljine a/2, a to je:";
+    document.getElementById("c").innerHTML = m/2;
+    document.getElementById("slika").src = "Pic.jpg";
+    
+}  

dc58e82c2dca65f9f14e68390f3dfd0ca9065c9c
domi15 <81708998+domi15@users.noreply.github.com>
2022-06-07 13:59:08 +0200

    js 6

--- js.js
+++ js.js
@@ -4,20 +4,20 @@ function WhatTheDogDoing() {
     var m = n
     var broj = String(m)
     o = m / 2
-    document.getElementById('duljina').innerHTML = m
-    document.getElementById('povrsina').innerHTML = m*m
-    document.getElementById("a").innerHTML = "Sada ćemo podijeliti kvadrat na četiri jednaka dijela"
+    document.getElementById('duljina').innerHTML = m;
+    document.getElementById('povrsina').innerHTML = m*m;
+    document.getElementById("a").innerHTML = "Sada ćemo podijeliti kvadrat na četiri jednaka dijela";
     document.getElementById("b").innerHTML = "Možemo vidjeti kako stranice AE i AH sada tangente kružnice te kako su obje duljine a/2, a to je:";
     while (x < 10) {
-        o = o + 10
-        var t = String(o)
-        broj = broj + t
-        x = x + 1
+        o = o + 10;
+        var t = String(o);
+        broj = broj + t;
+        x = x + 1;
     }
     document.getElementById("c").innerHTML = broj;
-    document.getElementById("d").innerHTML = "Duljina je"
-    document.getElementById("e").innerHTML = "Površina je"
-    document.getElementById("slika").src = "Pic.jpg"
+    document.getElementById("d").innerHTML = "Duljina je";
+    document.getElementById("e").innerHTML = "Površina je";
+    document.getElementById("slika").src = "Pic.jpg";
     
 }
 function DarkMode() {

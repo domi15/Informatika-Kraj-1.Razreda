f0ce65c3e44cdfe0dc5b7afa7dd39c9fab731422
domi15 <81708998+domi15@users.noreply.github.com>
2022-06-07 13:31:57 +0200

    js 3

--- js.js
+++ js.js
@@ -2,19 +2,19 @@ function WhatTheDogDoing() {
     x = 0
     n = prompt('Unesite najmanju duljinu stranice A')
     var m = n
-    num = m
+    var broj = num.toString(m)
     o = m / 2
     document.getElementById('duljina').innerHTML = m
     document.getElementById('povrsina').innerHTML = m*m
     document.getElementById("a").innerHTML = "Sada ćemo podijeliti kvadrat na četiri jednaka dijela"
     document.getElementById("b").innerHTML = "Možemo vidjeti kako stranice AE i AH sada tangente kružnice te kako su obje duljine a/2, a to je:";
-    while (counter % 2 != 2 && x < 10) {
+    while (x < 10) {
         o = o + 10
         var t = num.toString(o)
-        num = num + o
+        broj = broj + o
         var x = x + 1
     }
-    document.getElementById("c").innerHTML = num;
+    document.getElementById("c").innerHTML = broj;
     document.getElementById("d").innerHTML = "Duljina je"
     document.getElementById("e").innerHTML = "Površina je"
     document.getElementById("slika").src = "Pic.jpg"
